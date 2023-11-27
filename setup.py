f29747bf81eb9807ead341f6312d5cd446c8cb0f
Attaullah <2018327287@knu.ac.kr>
2023-11-28 01:35:26 +0900

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This porject will help in recommending top 3 food items',
+    author='Attaullah',
+    license='',
+)
